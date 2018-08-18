5c8d60a4db657c5d720afb8e3c7129ab2461359a
Raffael Tancman <raffael.tancman@resultadosdigitais.com.br>
2018-08-18 19:49:54 -0300

    feat: Change ListBooks to stateless component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,33 +3,32 @@ import PropTypes from 'prop-types'
 import Book from './Book'
 
 
-class ListBooks extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    onMoveShelf: PropTypes.func.isRequired
-  }
+const ListBooks = (props) => {
+  const { books, title, onMoveShelf} = props
 
-  render() {
-    const { books, title, onMoveShelf} = this.props
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-           {books.map((book) => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                  onMoveShelf={onMoveShelf}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                onMoveShelf={onMoveShelf}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+ListBooks.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  onMoveShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks
